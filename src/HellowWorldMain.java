696e5776009d34c2492cbc9069e3b452832a26ee
TadasSa <t.sabaitis@gmail.com>
2021-07-17 13:19:28 +0300

    initial commit

---
+++ src/HellowWorldMain.java
@@ -0,0 +1,11 @@
+public class HellowWorldMain {
+
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
+        String p = "Hello world !!";
+        System.out.println(p);
+    }
+}
